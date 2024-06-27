7712739c2bb9cd67bc90281bfcbc5872ebf249df
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-06-26 21:12:21 -0600

    login and logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,44 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { createAccessToken } from "../libs/jwt.js";
 
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userFound = await User.findOne({ email });
+
+    if (!userFound)
+      return res.status(400).json({
+        message: ["The email does not exist"],
+      });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: ["The password is incorrect"],
+      });
+    }
+
+    const token = await createAccessToken({
+      id: userFound._id,
+      username: userFound.username,
+    });
+
+    res.cookie("token", token, {
+      httpOnly: process.env.NODE_ENV !== "development",
+      secure: true,
+      sameSite: "none",
+    });
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
@@ -29,4 +67,11 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => res.send("login");
+export const logout = async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
