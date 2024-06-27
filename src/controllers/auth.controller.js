2d65d4bb88f1cd770eb3fd4a96c989083e30ec72
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-06-26 18:16:02 -0600

    add cript and token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,31 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
+
+  const passwordHash = await bcrypt.hash(password, 10);
 
   try {
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
 
     const userSaved = await newUser.save();
-    res.json(userSaved);
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createAt: userSaved.createdAt,
+      updateAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    send.error("error");
+    res.status(500).json({ message: error.message });
   }
 };
 
