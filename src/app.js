33a3da39c0bc12217b8c58f210f2c16e6befd4f1
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-07-11 18:55:56 -0600

    fix cors

--- src/app.js
+++ src/app.js
@@ -2,14 +2,19 @@ import express, { json } from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
 import NewProduct from "./routes/products.routes.js";
+import { FRONTEND_URL } from "./config.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: FRONTEND_URL,
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
