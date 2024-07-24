71851d447d728261be1a9cba6a7a966b7defac90
curcomil <sadielsalgado98@gmail.com>
2024-07-23 20:27:21 -0600

    fixing errors

--- src/models/pedidos.model.js
+++ src/models/pedidos.model.js
@@ -6,6 +6,8 @@ const PedidoSchema = new mongoose.Schema({
       producto: { type: String, required: true },
       cantidad: { type: Number, required: true },
       precio: { type: Number, required: true },
+      product_name: { type: String, required: true },
+      image: { type: String, required: true },
     },
   ],
   detalles_envio: {
@@ -13,7 +15,7 @@ const PedidoSchema = new mongoose.Schema({
   },
   numero_guia: { type: String },
   status_producto: { type: String },
-  total: { type: Number, required: true },
+  total: { type: Number },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   username_author: { type: String },
   pagado: { type: Boolean, default: false },
