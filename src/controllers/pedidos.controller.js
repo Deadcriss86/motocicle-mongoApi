401cf99e122aab7b22881063dc71c38024db50cb
curcomil <sadielsalgado98@gmail.com>
2024-07-24 20:35:35 -0600

    fix

--- src/controllers/pedidos.controller.js
+++ src/controllers/pedidos.controller.js
@@ -59,7 +59,7 @@ export const NewPedido = async (req, res) => {
 
 export const deletePedido = async (req, res) => {
   const { id } = req.params;
-
+  console.log("hola mundo");
   try {
     const pedido = await Pedido.findById(id);
 
