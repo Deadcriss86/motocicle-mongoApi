62886e0fa2debc2a1a0fba21e0f322df4ea5b228
curcomil <sadielsalgado98@gmail.com>
2024-08-06 20:49:38 -0600

    fix config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 3000;
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
