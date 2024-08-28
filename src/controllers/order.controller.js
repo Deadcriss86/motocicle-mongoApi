f6a140925c5eae129831545070bb7a03be7786ae
curcomil <sadielsalgado98@gmail.com>
2024-08-27 20:26:09 -0600

    add user info to order

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,4 +1,5 @@
 import Order from "../models/order.model.js";
+import User from "../models/user.model.js";
 
 // Obtener todas las órdenes
 export const GetAllOrders = async (req, res) => {
@@ -47,13 +48,18 @@ export const UpdateOrderById = async (req, res) => {
 export const GetOrderByAuthor = async (req, res) => {
   try {
     const authorId = req.user._id;
+    const author = await User.findById(authorId);
+    const author_adress = {
+      municipio: author.ciudad,
+      estado: author.delegacion,
+    };
     const order = await Order.find({ author: authorId });
 
     if (!order || order.length === 0) {
       return res.status(404).json({ message: "Orden no encontrada" });
     }
 
-    res.status(200).json(order);
+    res.status(200).json({ order, author_adress });
   } catch (error) {
     console.error("Error al obtener la orden:", error); // Añade un log para depuración
     res.status(500).json({ message: "Error al obtener la orden", error });
